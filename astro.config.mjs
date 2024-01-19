fc483a02d640c410afd370d5ee79d8a632b088b1
Bloodyaugust <greysonrichey@gmail.com>
2024-01-18 17:51:15 -0700

    Change to SSR mode so we don't need a separate server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
