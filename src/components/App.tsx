546db7ab8541958ce32f5ecb278d0535f31e8f33
Bloodyaugust <greysonrichey@gmail.com>
2024-01-18 22:28:04 -0700

    Mucking about with prettier and eslint

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,18 @@
-import type React from "react";
-import useName from "../hooks/UseName";
-import styles from "./App.module.css";
-import { useState } from "react";
-import useClientID from "../hooks/UseClientID";
-import TeamContextComponent from "../contexts/TeamContext";
-import TeamChooser from "./TeamChooser";
-import UserSidebar from "./UserSidebar";
+import type React from 'react';
+import { useState } from 'react';
+
+import TeamChooser from '@components/TeamChooser';
+import UserSidebar from '@components/UserSidebar';
+import TeamContextComponent from '@contexts/TeamContext';
+import useClientID from '@hooks/UseClientID';
+import useName from '@hooks/UseName';
+
+import styles from './App.module.css';
 
 export default function App() {
   const { name, setName } = useName();
   const clientID = useClientID();
-  const [newName, setNewName] = useState<string>("");
+  const [newName, setNewName] = useState<string>('');
 
   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNewName(event.target.value);
@@ -18,11 +20,24 @@ export default function App() {
 
   const handleNameSubmit = () => {
     setName(newName);
-    setNewName("");
+    setNewName('');
   };
 
   if (!clientID) {
     return <span>Loading...</span>;
+  }
+
+  if (!name) {
+    return (
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <h2>What name do you want to use?</h2>
+          <label>Name: </label>
+          <input onChange={handleNameChange} />
+          <button onClick={handleNameSubmit}>Let&apos;s Go!</button>
+        </div>
+      </div>
+    );
   }
 
   return (
@@ -30,7 +45,7 @@ export default function App() {
       <div className={styles.container}>
         <div className={styles.content}>
           {name ? (
-            <span>Hello {name}!</span>
+            <span>Hello, {name}!</span>
           ) : (
             <>
               <label>Name: </label>
