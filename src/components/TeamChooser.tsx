2117421583177438ab14045a97c307fb74be2233
Bloodyaugust <greysonrichey@gmail.com>
2024-01-20 15:23:18 -0700

    Add team deep-linking

--- src/components/TeamChooser.tsx
+++ src/components/TeamChooser.tsx
@@ -1,9 +1,10 @@
-import { useContext, useState } from "react";
-import { TeamContext } from "../contexts/TeamContext";
-import pocketBase from "../lib/pocketbase";
-import type { Team } from "../types/team";
-import styles from "./TeamChooser.module.css";
-import { TeamState } from "../types/teamState";
+import { useContext, useEffect, useState } from 'react';
+
+import { TeamContext } from '../contexts/TeamContext';
+import pocketBase from '../lib/pocketbase';
+import type { Team } from '../types/team';
+import { TeamState } from '../types/teamState';
+import styles from './TeamChooser.module.css';
 
 type Props = {
   clientID: string;
@@ -12,22 +13,23 @@ type Props = {
 export default function TeamChooser({ clientID }: Props) {
   const { teamID, setTeamID } = useContext(TeamContext);
 
-  const [teamName, setTeamName] = useState("");
-  const [joinTeamID, setJoinTeamID] = useState("");
+  const [teamName, setTeamName] = useState('');
+  const [joinTeamID, setJoinTeamID] = useState('');
   const [joinTeamError, setJoinTeamError] = useState<string | null>(null);
+  const [loadParams, setLoadParams] = useState<Record<string, string>>({});
 
   const handleTeamNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTeamName(event.target.value);
   };
 
   const handleJoinTeamIDChange = (
-    event: React.ChangeEvent<HTMLInputElement>
+    event: React.ChangeEvent<HTMLInputElement>,
   ) => {
     setJoinTeamID(event.target.value);
   };
 
   const handleCreateTeam = async () => {
-    const newTeam = (await pocketBase.collection("teams").create({
+    const newTeam = (await pocketBase.collection('teams').create({
       name: teamName,
       state: TeamState.POINTING,
       adminClientID: clientID,
@@ -38,18 +40,37 @@ export default function TeamChooser({ clientID }: Props) {
     setTeamID(newTeam.id);
   };
 
-  const handleJoinTeam = async () => {
+  const handleJoinTeam = async (deepLinkID: string | undefined) => {
+    const joiningTeam: string = deepLinkID || joinTeamID;
+
     try {
       const team = (await pocketBase
-        .collection("teams")
-        .getOne(joinTeamID)) as Team;
+        .collection('teams')
+        .getOne(joiningTeam)) as Team;
 
       setTeamID(team.id);
       setJoinTeamError(null);
     } catch {
-      setJoinTeamError("Team does not exist");
+      setJoinTeamError('Team does not exist');
     }
   };
+
+  useEffect(() => {
+    if (clientID) {
+      const urlSearchParams = new URLSearchParams(window.location.search);
+      setLoadParams(Object.fromEntries(urlSearchParams.entries()));
+    }
+  }, [clientID]);
+
+  useEffect(() => {
+    if (clientID) {
+      const { team } = loadParams;
+
+      if (team) {
+        handleJoinTeam(team);
+      }
+    }
+  }, [loadParams]);
 
   if (teamID) {
     return undefined;
@@ -66,7 +87,13 @@ export default function TeamChooser({ clientID }: Props) {
       <label>Team ID:</label>
       <input onChange={handleJoinTeamIDChange} />
       {joinTeamError && <span className={styles.error}>{joinTeamError}</span>}
-      <button onClick={handleJoinTeam}>Join Team</button>
+      <button
+        onClick={() => {
+          handleJoinTeam(undefined);
+        }}
+      >
+        Join Team
+      </button>
     </div>
   );
 }
--- src/components/sidebar/TeamID.tsx
+++ src/components/sidebar/TeamID.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useCallback, useContext, useMemo } from 'react';
 
 import { TeamContext } from '@contexts/TeamContext';
 
@@ -6,6 +6,20 @@ import styles from './TeamID.module.css';
 
 export default function TeamID() {
   const { team } = useContext(TeamContext);
+
+  const inviteLink = useMemo<string | null>(() => {
+    if (!team) {
+      return null;
+    }
+
+    return `https://point-collab-production.up.railway.app/?team=${team.id}`;
+  }, [team]);
+
+  const handleCopyInviteLinkClick = useCallback(() => {
+    if (inviteLink) {
+      navigator.clipboard.writeText(inviteLink);
+    }
+  }, [inviteLink]);
 
   if (!team) {
     return undefined;
@@ -17,7 +31,10 @@ export default function TeamID() {
         <span className={styles.teamIDHeader}>Team ID:</span>
         <span className={styles.teamIDValue}>{team.id}</span>
       </div>
-      <div className={styles.teamLinkContainer}>
+      <div
+        className={styles.teamLinkContainer}
+        onClick={handleCopyInviteLinkClick}
+      >
         <span className="material-icons">link</span>
         <span className={styles.teamIDLink}>Copy link to invite</span>
       </div>
