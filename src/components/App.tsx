ab9ee68a95d536a18dacaf242c4a3f53b20a2ad2
Bloodyaugust <greysonrichey@gmail.com>
2024-01-20 21:46:00 -0700

    Finish onboarding flow

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,10 +2,10 @@ import type React from 'react';
 import { useState } from 'react';
 
 import AdminSidebar from '@components/AdminSidebar';
-import TeamChooser from '@components/TeamChooser';
 import UserSidebar from '@components/UserSidebar';
 import AdminMain from '@components/main/AdminMain';
 import PointGrid from '@components/main/PointGrid';
+import Welcome from '@components/main/Welcome';
 import TeamID from '@components/sidebar/TeamID';
 import TeamContextComponent from '@contexts/TeamContext';
 import useClientID from '@hooks/UseClientID';
@@ -27,6 +27,10 @@ export default function App() {
     setNewName('');
   };
 
+  const handleOnboarded = (newClientName: string) => {
+    setName(newClientName);
+  };
+
   if (!clientID) {
     return <span>Loading...</span>;
   }
@@ -34,12 +38,7 @@ export default function App() {
   if (!name) {
     return (
       <div className={styles.container}>
-        <div className={styles.content}>
-          <h2>What name do you want to use?</h2>
-          <label>Name: </label>
-          <input onChange={handleNameChange} />
-          <button onClick={handleNameSubmit}>Let&apos;s Go!</button>
-        </div>
+        <Welcome clientID={clientID} onOnboarded={handleOnboarded} />
       </div>
     );
   }
@@ -55,7 +54,6 @@ export default function App() {
               <button onClick={handleNameSubmit}>Submit</button>
             </>
           )}
-          <TeamChooser clientID={clientID} />
           <PointGrid />
           <AdminMain clientID={clientID} />
         </div>
---
+++ src/components/main/Welcome.tsx
@@ -0,0 +1,184 @@
+import clsx from 'clsx';
+import { useEffect, useState } from 'react';
+
+import sharedStyles from '@components/SharedStyles.module.css';
+import pocketBase from '@lib/pocketbase';
+import type { Team } from '@projectTypes/team';
+import { TeamState } from '@projectTypes/teamState';
+
+import styles from './Welcome.module.css';
+
+enum TeamMode {
+  NONE,
+  JOIN,
+  START,
+}
+
+type Props = {
+  clientID: string;
+  onOnboarded: (clientName: string) => void;
+};
+
+export default function Welcome({ clientID, onOnboarded }: Props) {
+  const [clientName, setClientName] = useState<string | null>(null);
+  const [teamID, setTeamID] = useState<string | null>(null);
+  const [teamMode, setTeamMode] = useState<TeamMode>(TeamMode.NONE);
+  const [teamName, setTeamName] = useState('');
+  const [joinTeamID, setJoinTeamID] = useState('');
+  const [joinTeamError, setJoinTeamError] = useState<string | null>(null);
+  const [loadParams, setLoadParams] = useState<Record<string, string>>({});
+
+  const storeTeamID = () => {
+    if (teamID) {
+      localStorage.setItem('teamID', teamID);
+    }
+  };
+
+  const handleTeamNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTeamName(event.target.value);
+  };
+
+  const handleJoinTeamIDChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setJoinTeamID(event.target.value);
+  };
+
+  const handleCreateTeam = async () => {
+    const newTeam = (await pocketBase.collection('teams').create({
+      name: teamName,
+      state: TeamState.POINTING,
+      adminClientID: clientID,
+      points: [],
+      history: [],
+    })) as Team;
+
+    setTeamID(newTeam.id);
+  };
+
+  const handleJoinTeam = async (deepLinkID: string | undefined) => {
+    const joiningTeam: string = deepLinkID || joinTeamID;
+
+    try {
+      const team = (await pocketBase
+        .collection('teams')
+        .getOne(joiningTeam)) as Team;
+
+      setTeamID(team.id);
+      setJoinTeamError(null);
+    } catch {
+      setJoinTeamError('Team does not exist');
+    }
+  };
+
+  useEffect(() => {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    setLoadParams(Object.fromEntries(urlSearchParams.entries()));
+  }, []);
+
+  useEffect(() => {
+    const { team } = loadParams;
+
+    if (team) {
+      handleJoinTeam(team);
+    }
+  }, [loadParams]);
+
+  useEffect(() => {
+    if (clientName && teamID) {
+      storeTeamID();
+      onOnboarded(clientName);
+    }
+  }, [clientName, teamID]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.welcomeContainer}>
+        <h2>Welcome!</h2>
+        <hr />
+      </div>
+      <div className={styles.nameContainer}>
+        <label className={sharedStyles.label}>What is your name?</label>
+        <input
+          className={sharedStyles.inputText}
+          type="text"
+          onChange={(e) => {
+            setClientName(e.currentTarget.value);
+          }}
+          placeholder="Enter name you'd like displayed to the room"
+        />
+      </div>
+      <div className={styles.buttonsContainer}>
+        <label className={sharedStyles.label}>What would you like to do?</label>
+        <div className={styles.buttonsInnerContainer}>
+          <button
+            className={clsx(
+              sharedStyles.button,
+              teamMode === TeamMode.JOIN && styles.active,
+            )}
+            disabled={!clientName}
+            onClick={() => {
+              setTeamMode(
+                teamMode === TeamMode.JOIN ? TeamMode.NONE : TeamMode.JOIN,
+              );
+            }}
+          >
+            Join an existing team
+          </button>
+          <button
+            className={clsx(
+              sharedStyles.button,
+              teamMode === TeamMode.START && styles.active,
+            )}
+            disabled={!clientName}
+            onClick={() => {
+              setTeamMode(
+                teamMode === TeamMode.START ? TeamMode.NONE : TeamMode.START,
+              );
+            }}
+          >
+            Start a new team
+          </button>
+        </div>
+      </div>
+      {teamMode === TeamMode.JOIN && (
+        <div className={styles.teamIDContainer}>
+          <label className={sharedStyles.label}>Enter your Team ID</label>
+          <input
+            className={sharedStyles.inputText}
+            type="text"
+            onChange={handleJoinTeamIDChange}
+            placeholder="Team ID"
+          />
+          {joinTeamError && (
+            <span className={sharedStyles.error}>{joinTeamError}</span>
+          )}
+          <button
+            className={sharedStyles.button}
+            onClick={() => {
+              handleJoinTeam(undefined);
+            }}
+          >
+            Join Team!
+          </button>
+        </div>
+      )}
+      {teamMode === TeamMode.START && (
+        <div className={styles.teamNameContainer}>
+          <label className={sharedStyles.label}>
+            What is the name of your team?
+          </label>
+          <input
+            className={sharedStyles.inputText}
+            type="text"
+            onChange={handleTeamNameChange}
+            placeholder="Team Awesome"
+          />
+          <button className={sharedStyles.button} onClick={handleCreateTeam}>
+            Start Team!
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
