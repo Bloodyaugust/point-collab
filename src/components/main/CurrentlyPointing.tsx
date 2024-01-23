05d6cdb4a4024e211c0e3e6546bb7ba9e54f6b52
Bloodyaugust <greysonrichey@gmail.com>
2024-01-22 17:38:45 -0700

    Add support for link clicking/id differentiation

--- src/components/main/CurrentlyPointing.tsx
+++ src/components/main/CurrentlyPointing.tsx
@@ -1,11 +1,22 @@
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 
 import { TeamContext } from '@contexts/TeamContext';
 
 import styles from './CurrentlyPointing.module.css';
 
+const urlRegex =
+  /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gi;
+
 export default function CurrentlyPointing() {
   const { team } = useContext(TeamContext);
+
+  const storyIsLink = useMemo<boolean>(() => {
+    if (!team || !team.storyID) {
+      return false;
+    }
+
+    return urlRegex.test(team.storyID);
+  }, [team]);
 
   if (!team) {
     return undefined;
@@ -14,7 +25,13 @@ export default function CurrentlyPointing() {
   return (
     <div className={styles.container}>
       <span className={styles.pointingMessage}>Currently Pointing:</span>
-      <span className={styles.pointingStory}>{team.storyID}</span>
+      {storyIsLink ? (
+        <a className={styles.pointingStory} href={team.storyID}>
+          {team.storyID}
+        </a>
+      ) : (
+        <span className={styles.pointingStory}>{team.storyID}</span>
+      )}
     </div>
   );
 }
